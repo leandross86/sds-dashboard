140a13f6823f07800787c595d05e43db20a790a5
leandrobko <leandrobko@gmail.com>
2021-05-03 23:34:24 -0300

    Project created

---
+++ backend/src/main/java/com/bkoinfo/bkovendas/BkovendasApplication.java
@@ -0,0 +1,13 @@
+package com.bkoinfo.bkovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkovendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bkoinfo/bkovendas/BkovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkoinfo.bkovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
