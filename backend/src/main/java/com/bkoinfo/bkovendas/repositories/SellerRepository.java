9a5ee72aaf4e5300e993ad2e2b25d9fb989e7133
leandrobko <leandrobko@gmail.com>
2021-05-06 20:33:51 -0300

    Layers

---
+++ backend/src/main/java/com/bkoinfo/bkovendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.bkoinfo.bkovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bkoinfo.bkovendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
