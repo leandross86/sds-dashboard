869cf39fe05ebc1d94d0740618852f0a75611c6f
leandrobko <leandrobko@gmail.com>
2021-05-06 21:40:15 -0300

    Pagination

---
+++ backend/src/main/java/com/bkoinfo/bkovendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.bkoinfo.bkovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bkoinfo.bkovendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
