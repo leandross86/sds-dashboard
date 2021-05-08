22013730c1b305d1143b0fd10b2bce2f368a6503
leandrobko <leandrobko@gmail.com>
2021-05-07 21:25:24 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="py-3">
-          <h2 className="text-primary">Todas Vendas</h2>primary
+          <h2 className="text-primary">Todas Vendas</h2>
         </div>
         <DataTable />
       </div>
